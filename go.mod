fbc098426c4fad1ed8c4fa98b9de39e761bd13fa
Teymur Gahramanov <gahramanovteymur@gmail.com>
2024-01-19 16:37:30 +0400

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module check-from-node-exporter
+
+go 1.21.6
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Target represents the structure of the YAML data
+type Target struct {
+	Address string `yaml:"address"`
+	Type string `yaml:"type"`
+}
+
+// config represents the dynamic expansion of targets
+type Config struct {
+	Targets []map[string]Target `yaml:"targets"`
+}
+
+func main() {
+	// Read YAML file
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	// Unmarshal YAML data into config
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Access and print values
+	for _, targetMap := range config.Targets {
+		for key, value := range targetMap {
+			fmt.Printf("Target: %s\n", key)
+			fmt.Printf("Address: %s\n", value.Address)
+			fmt.Printf("Type: %s\n", value.Type)
+		}
+	}
+}
