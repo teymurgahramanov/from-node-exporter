a929ac76d1ae9e74c223dca7c6e9e750530b58ed
Teymur <teymur_gahramanov@gmail.com>
2024-02-11 11:44:11 +0400

    use pinger

--- modules/icmp.go
+++ modules/icmp.go
@@ -1,32 +1,18 @@
 package modules
 
 import (
-	"net"
+	probing "github.com/prometheus-community/pro-bing"
 )
 
-func ProbeICMP(address string) (bool,error) {
-	ipAddr, err := net.ResolveIPAddr("ip", address)
+func ProbeICMP(address string) (bool, error) {
+	pinger, err := probing.NewPinger(address)
 	if err != nil {
 		return false, err
 	}
-
-	conn, err := net.DialIP("ip4:icmp", nil, ipAddr)
+	pinger.Count = 3
+	err = pinger.Run()
 	if err != nil {
 		return false, err
 	}
-	defer conn.Close()
-
-	msg := []byte("hello")
-	_, err = conn.Write(msg)
-	if err != nil {
-		return false, err
-	}
-
-	reply := make([]byte, 1024)
-	_, err = conn.Read(reply)
-	if err != nil {
-		return false, err
-	}
-
 	return true, nil
 }
